ee58c0d1d9a2e0a1e8b9407f6ac6baccabb1a085
Sushant.k.bhaskar <sushantkbhaskar5@gmail.com>
2026-04-21 22:51:57 +0530

    new add

---
+++ src/main/java/org/example/ArrayPractice3SecondMaxElement.java
@@ -0,0 +1,34 @@
+package org.example;
+import java .util.*;
+
+public class ArrayPractice3SecondMaxElement {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int max;
+        int secondMax = Integer.MIN_VALUE;
+        System.out.println("Enter the size of the array: ");
+        n= sc.nextInt();
+        int []arr = new int[n];
+        System.out.println("Enter the Array Elements: ");
+        for(int i = 0 ; i<n ;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array Elements are : ");
+        for(int i = 0 ; i<n ;i++){
+            System.out.println(arr[i]);
+        }
+        max = arr[0];
+        for(int i =1 ; i < n ;i++){
+            if(arr[i] > max){
+                secondMax = max;
+                max = arr[i];
+            } else if (arr[i] > secondMax && arr[i]!=max) {
+                secondMax = arr[i];
+            }
+        }
+        System.out.println("Max Array element is : "+ max);
+        System.out.println("Second Max Array element is : "+ secondMax);
+    }
+
+}
