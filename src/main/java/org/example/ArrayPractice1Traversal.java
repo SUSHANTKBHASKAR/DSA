27dcde77252fad69f6e0cfb927703d895d17313e
Sushant.k.bhaskar <sushantkbhaskar5@gmail.com>
2026-04-21 22:14:48 +0530

    new add

--- src/main/java/org/example/ArrayPractice1Traversal.java
+++ src/main/java/org/example/ArrayPractice1Traversal.java
@@ -1,7 +1,7 @@
 package org.example;
 import java.util.*;
 
-public class ArrayPractice1 {
+public class ArrayPractice1Traversal {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n;
