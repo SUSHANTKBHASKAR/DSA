86879aecbd3261c55445c9381baa74d98196ec13
Sushant.k.bhaskar <sushantkbhaskar5@gmail.com>
2026-04-21 22:10:16 +0530

    new add

---
+++ src/main/java/org/example/ArrayPractice1.java
@@ -0,0 +1,20 @@
+package org.example;
+import java.util.*;
+
+public class ArrayPractice1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the value of n: ");
+        n= sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the array elements: ");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Array Elements are: ");
+        for(int i=0;i<n;i++){
+            System.out.println(arr[i] +" index is: "+ (i+1));
+        }
+    }
+}
