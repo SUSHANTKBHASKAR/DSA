55bbd8468ab94e80bbaa105eb4dbf74955ecc7f0
Sushant.k.bhaskar <sushantkbhaskar5@gmail.com>
2026-05-02 12:23:54 +0530

    secondMax3.0

---
+++ src/main/java/org/example/SecondLargest.java
@@ -0,0 +1,43 @@
+package org.example;
+import java.util.Scanner;
+public class SecondLargest {
+    public int getSecondLargest(int[]arr , int n){
+        if(n<2){
+            return -1;
+        }
+        int max;
+        int secondMax = Integer.MIN_VALUE;
+
+        max=arr[0];
+
+        for(int i=1;i<n;i++){
+            if(arr[i]>max){
+                secondMax=max;
+                max=arr[i];
+            } else if (arr[i]>secondMax && arr[i]!=max) {
+                secondMax=arr[i];
+            }
+        }
+        return (secondMax== Integer.MIN_VALUE) ? -1 :secondMax;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Enter the value of n: ");
+        n= sc.nextInt();
+        int []arr = new int[n];
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the elements : " + (i+1));
+            arr[i]= sc.nextInt();
+        }
+        System.out.println("Array is : ");
+        for(int i=0 ;i<n ;i++){
+            System.out.println(arr[i]);
+        }
+        SecondLargest obj = new SecondLargest();
+        int result = obj.getSecondLargest(arr, n);
+
+        System.out.println("Result: "+ result);
+    }
+}
